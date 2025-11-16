384ad769ded24526821115659f2d3912f161ef02
EggyMath <55564321+EggyMath@users.noreply.github.com>
2025-11-15 21:31:44 -0500

    Screen Time Check

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:usage_stats/usage_stats.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,13 +31,53 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   int _counter = 0;
+  Duration _screenTime = Duration.zero;
+
+  @override
+  void initState() {
+    super.initState();
+    _getScreenTimeToday();
+  }
+
+  Future<void> _getScreenTimeToday() async {
+    final now = DateTime.now();
+    final startOfDay = DateTime(now.year, now.month, now.day);
+
+    // Check permission
+    bool granted = await UsageStats.checkUsagePermission() ?? false;
+    if (!granted) {
+      await UsageStats.grantUsagePermission();
+      // The user must manually grant permission in settings
+    }
+
+    List<UsageInfo> usage = await UsageStats.queryUsageStats(startOfDay, now);
+
+    int totalMillis = usage.fold<int>(
+      0,
+          (int sum, info) {
+        int time = ((info.totalTimeInForeground as num?)?.toInt() ?? 0);
+        return sum + time;
+      },
+    );
+
+    setState(() {
+      _screenTime = Duration(milliseconds: totalMillis);
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
+  }
+
+  String get formattedScreenTime {
+    final hours = _screenTime.inHours;
+    final minutes = _screenTime.inMinutes % 60;
+    final seconds = _screenTime.inSeconds % 60;
+    return '${hours}h ${minutes}m ${seconds}s';
   }
 
   @override
@@ -54,6 +95,12 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Screen time today: $formattedScreenTime',
+              style: Theme.of(context).textTheme.titleMedium,
+              textAlign: TextAlign.center,
             ),
           ],
         ),
